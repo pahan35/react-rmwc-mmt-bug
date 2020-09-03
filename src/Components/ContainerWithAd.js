0917ae0ad6f3792b4cf4e30b19a0b4b83dc717aa
Pavlo Zhukov <pahazhukov@gmail.com>
2020-09-03 23:08:58 +0300

    feat: imitate open/close sidebar behavior

--- src/Components/ContainerWithAd.js
+++ src/Components/ContainerWithAd.js
@@ -1,20 +1,19 @@
 import AdSlot1 from 'Components/AdSlot1'
-import AppContext from 'Components/AppContext'
 import {Button, Card} from 'mdc'
-import React, {useContext, useState} from 'react'
+import React, {useState} from 'react'
 
 export default function ContainerWithAd() {
-  const {setUser} = useContext(AppContext)
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(true)
   return (
     <Card>
-      Count is {count}
-      <AdSlot1 />
-      <Button onClick={() => setUser({hasSubscription: true})}>
-        Sign in as subscribed user
-      </Button>
-      <Button onClick={() => setCount(prevCount => prevCount + 1)}>
-        Increase
+      {open && (
+        <aside>
+          I&apos;m sidebar
+          <AdSlot1 />
+        </aside>
+      )}
+      <Button onClick={() => setOpen(prevOpen => !prevOpen)}>
+        {open ? 'Hide' : 'Show'} Sidebar
       </Button>
     </Card>
   )
