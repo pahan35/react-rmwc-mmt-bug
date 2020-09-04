3487a5be6fa23d63a29298342e11bd797f981e5b
Pavlo Zhukov <pahazhukov@gmail.com>
2020-09-04 11:10:56 +0300

    feat: use real non-repeatable slot id

--- src/Components/AdSlot1.js
+++ src/Components/AdSlot1.js
@@ -2,5 +2,5 @@ import MMTSlot from 'Components/MMTSlot'
 import React from 'react'
 
 export default function AdSlot1() {
-  return <MMTSlot slotId="1" />
+  return <MMTSlot slotId="c888b21f-9e7c-47ef-8154-839712819b85" />
 }
