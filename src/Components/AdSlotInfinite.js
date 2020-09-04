ca9c52375d771d3250969d767795ad5c4ff35526
Pavlo Zhukov <pahazhukov@gmail.com>
2020-09-04 11:04:44 +0300

    feat: imitate append page behavior with infinite slot

---
+++ src/Components/AdSlotInfinite.js
@@ -0,0 +1,11 @@
+import MMTSlot from 'Components/MMTSlot'
+import React from 'react'
+
+export default function AdSlotInfinite() {
+  return (
+    <MMTSlot
+      infiniteSlotId="INF9945fff5ad15"
+      slotId="cd26041f-d2b5-40b0-a277-9945fff5ad15"
+    />
+  )
+}
--- src/Components/ContainerWithAd.js
+++ src/Components/ContainerWithAd.js
@@ -1,9 +1,24 @@
 import AdSlot1 from 'Components/AdSlot1'
+import AdSlotInfinite from 'Components/AdSlotInfinite'
 import {Button, Card} from 'mdc'
-import React, {useState} from 'react'
+import React, {Fragment, useState} from 'react'
+
+function Pages({pages}) {
+  const renderedPages = []
+  for (let i = 1; i <= pages; i += 1) {
+    renderedPages.push(
+      <Fragment key={i}>
+        Page {i}
+        <AdSlotInfinite />
+      </Fragment>,
+    )
+  }
+  return renderedPages
+}
 
 export default function ContainerWithAd() {
   const [open, setOpen] = useState(true)
+  const [pages, setPages] = useState(1)
   return (
     <Card>
       {open && (
@@ -14,6 +29,10 @@ export default function ContainerWithAd() {
       )}
       <Button onClick={() => setOpen(prevOpen => !prevOpen)}>
         {open ? 'Hide' : 'Show'} Sidebar
+      </Button>
+      <Pages pages={pages} />
+      <Button onClick={() => setPages(prevPages => prevPages + 1)}>
+        Add page
       </Button>
     </Card>
   )
